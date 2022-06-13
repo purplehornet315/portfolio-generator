7920540df249606df8282d7f1101013884ab0bf3
Taylor Leverette <kyle_taylor315@hotmail.com>
2022-06-12 21:49:06 -0500

    section 9.1

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var commandLineArgs = process.agrv;
-console.log(commandLineArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+const printProfileData = profileDataArgs => {
+    // This...
+    for(let i=0; i< profileDataArgs.length; i++){
+        console.log(profileDataArgs[i]);
+    }
+    console.log('================')
+
+    // is the same as this...
+    profileDataArgs.forEach((profileItem) => {
+        console.log(profileItem);
+    });
+};
+
+printProfileData(profileDataArgs);
